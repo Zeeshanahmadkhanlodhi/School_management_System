16ccf490ea16206a4d51b99442607c0795f35845
zeeshanahmadkhanlodhi <zeeshan22ahmad33@gmail.com>
2022-07-24 10:23:37 -0700

    Authentication

---
+++ Student_management_system/Auth/forget_password.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/Style/main.css">
+    <title>SMS | Forget Password</title>
+</head>
+<body>
+    <div class="auth">
+        <form class="auth_form">
+            <h1>Admin   |  Forget Password</h1>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" placeholder="Email">
+            <input type="submit" value="Forget Password">
+        </form>
+    </div>
+</body>
+</html>
